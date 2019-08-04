f030d6826d35a82eb5b08e747936e8aeb29f678d
manuelitox <manuel2788@gmail.com>
2019-08-04 02:01:32 +0200

    re-generate automatically choices list depending of the current path

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const fs = require("fs");
+const glob = require('glob');
 const path = require("path");
 const program = require("commander");
 const moment = require("moment");
 const inquirer = require('inquirer');
-const glob = require('glob');
+const EventEmitter = require('events');
 
-// TODOS: 
-// - Install and integrate inquirer package.
-// - get resources (directories and files) of a path.
-// - display resources list in inquirer.
-// - Allows select a resource
+const formattedFileSystem = fileSystem =>
+  fileSystem.map((file, index) => ({
+    key: index,
+    name: path.basename(file),
+    value: file
+  }));
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('event', filesPath => {
+  filesPath.unshift('Back to Previous level');
+  console.clear();
+  receiver(filesPath);
+});
 
 // TODOS:
 // move to helpers directory
@@ -19,44 +29,46 @@ const ResourceType = stats => stats.isDirectory() ? 'directory' : 'file';
 // stats function should be in own file.
 const stats = pathResolved => {
   const fileStats = fs.statSync(pathResolved);
-  // TODOS:
-  // missing error function.
 
   // TODOS:
   // refactor size function
   if (ResourceType(fileStats) === 'directory') {
-    console.log("open directory");
+    const filesPath = glob.sync(`${pathResolved}/*`);
+    myEmitter.emit('event', filesPath);
   } else {
     console.log("size", `${fileStats["size"]} bytes`)
   }
 
-  // TODOS: 
-  // refactor last modify time function
-  console.log("last modify time:", moment(fileStats["mtime"]).fromNow());
-  
+  console.log("last modify time:", moment(fileStats["mtime"]).fromNow());  
   console.log('Resource type:', ResourceType(fileStats));
 };
 
-const receiver = () => {
-  const currentFileSystem = glob.sync(`${process.cwd()}/*`);
-  const formattedFileSystem = currentFileSystem.map(file => path.basename(file));
-  
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'resource',
-        message: 'Select a resource',
-        choices: formattedFileSystem
-      }
-    ])
-    .then(answers => {
-      const pathResolved = path.resolve(answers.resource);
-      return stats(pathResolved); 
-    });
+const askQuestions = filesSystem => { 
+  const questions = [
+    {
+      type: 'list',
+      name: 'resource',
+      message: 'Select a resource',
+      choices: formattedFileSystem(filesSystem)
+    }
+  ];
+  return inquirer.prompt(questions);
+}
+
+const receiver = async (filesSystem) => { 
+  let answers = await askQuestions(filesSystem);
+  // TODOS: 
+  // - back to previous level only works in 2 first times. Later prints an error message
+  if (answers.resource === 'Back to Previous level') {
+    filesSystem.shift();
+    const splitPath = filesSystem[0].split("/");
+    const strippedPath = splitPath.slice(0, splitPath.length-2).join("/");
+    answers = await askQuestions(glob.sync(`${strippedPath}/*`));
+  }
+  return stats(answers.resource);
 };
 
 program
   .version('1.0.0')
-  .action(receiver)
+  .action(() => receiver(glob.sync(`${process.cwd()}/*`)))
   .parse(process.argv);
