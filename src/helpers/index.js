d4774835e5bf076d4f8ceb3871a19676226ae812
manuelitox <manuel2788@gmail.com>
2019-08-20 21:44:16 +0200

    create choice: back to root

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,9 @@
 const ResourceType = require("./resource-type");
+const generateBackToRoot = require("./generate-back-to-root");
 const formattedFileSystem = require("./formatted-file-system");
 
 module.exports = {
   ResourceType,
+  generateBackToRoot,
   formattedFileSystem
 };
