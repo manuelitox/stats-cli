8a67b41cf492e6a7749e5d1fbfc99318cb3a079e
manuelitox <manuel2788@gmail.com>
2019-09-24 20:00:00 +0200

    standardize code

--- src/helpers/resource-type.js
+++ src/helpers/resource-type.js
@@ -3,5 +3,6 @@ export const resourceType = stats => {
   if (typeof stats !== "object") return "stats has to be an object";
   if (!stats.isDirectory) return "stats.isDirectory doesnt exists";
   if (typeof stats.isDirectory !== "function") return "stats.isDirectory is not a function";
+  
   return stats.isDirectory() ? 'directory' : 'file';
 };
